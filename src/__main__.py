1c83fc1152c4902b83b4492ce62cdaf979adab9c
zxc monstr <exradicalmusic@gmail.com>
2026-02-26 22:07:26 +0300

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,25 @@
 from src.gui.main_window import MainWindow
-=======
-﻿from src.gui.main_window import MainWindow
->>>>>>> 5916909 (Sprint 1: Добавил файл для шифр. до вставки)
+from src.core.events import EventBus
+from src.database.db import Database
+from src.core.audit_logger import AuditLogger
+
 
 def main():
+    # База (пока просто app.db рядом с проектом)
+    db = Database("app.db")
+    db.migrate()
+
+    # Шина событий
+    bus = EventBus()
+
+    # Заглушка аудита подписывается на события
+    audit = AuditLogger(db)
+    audit.subscribe(bus)
+
+    # Запуск GUI (позже прокинем bus/db внутрь окна)
     app = MainWindow()
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
